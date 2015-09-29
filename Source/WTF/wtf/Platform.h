18f280b1842f3cb49fb4b17b03081b46d311d428
Daniel Schepler <dschepler@gmail.com>
2015-09-29 11:31:34 +0000

    Fix FTBFS in x32
    ===================================================================
    
    Gbp-Pq: Name x32_support.patch

--- Source/WTF/wtf/Platform.h
+++ Source/WTF/wtf/Platform.h
@@ -182,7 +182,11 @@
 /* CPU(X86_64) - AMD64 / Intel64 / x86_64 64-bit */
 #if   defined(__x86_64__) \
     || defined(_M_X64)
+#ifdef __ILP32__
+#define WTF_CPU_X86_64_32 1
+#else
 #define WTF_CPU_X86_64 1
+#endif
 #endif
 
 /* CPU(ARM64) - Apple */
